1cd30cf31c313aa64522555d9ef4881082025e95
RITIKMATHUR442 <ritikm335@gmail.com>
2025-01-02 17:27:43 +0530

    .

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
       <div className="footer-content">
         <p>© 2024 Ritik Mathur. All Rights Reserved.</p>
         <div className="social-links">
-          <a href="https://github.com/RITIKMATHUR442/callbackhell-project" target="_blank" rel="noopener noreferrer" className="social-link">
+          <a href="https://github.com/RITIKMATHUR442/Online-Library-System" target="_blank" rel="noopener noreferrer" className="social-link">
             <img src="/ngitbub.png" alt="GitHub" className="social-icon" />
           </a>
           <a href="https://www.instagram.com/mr_mathur.902?igsh=MTI0OXhsYnBiNDU2eA==" target="_blank" rel="noopener noreferrer" className="social-link">
